b427df83d2cd914bdc95d22eb1a05ed1a916f2ed
bonheurKhmr <bonheurkahamire8@gmail.com>
2024-05-03 02:28:29 +0200

    modification du app pour l'encement de server

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('bonheur ...');
+/** creation du port */
+
+const express = require("express")
+const app = express()
+const port = 3000
+
+/** les endpoint */
+app.get('/', (req, res) =>  res.send('hello Express'))
+
+/** lancement du server */
+app.listen(port, () => `l'application redemer sur le port : http://localhost:${port}`)
